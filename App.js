f3ed5b22865e909b2ce69be56a15c594d0472ed5
Yassir <hartaniyassir@gmail.com>
2018-11-30 22:02:19 +0100

    add a provider and a consumer

--- App.js
+++ App.js
@@ -9,40 +9,69 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
-const MyContext = React.createContext()
-export default class App extends Component {
+// create a context
+const MyContext = React.createContext();
+
+// create a provider
+class MyProvider extends Component {
   state = {
-    name: 'yassir',
+    name: "yassir",
     age: 20,
     cool: true
-  }
+  };
 
-  render(){
-    return(
-      <View style={styles.container}>
-        <Family name={this.state.name}/>
-      </View>
-    )
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          growYearOlder: () => {
+            this.setState({ age: this.state.age + 1 });
+          }
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
   }
 }
-
+export default class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <View style={styles.container}>
+          <Family />
+        </View>
+      </MyProvider>
+    );
+  }
+}
 
 class Person extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <View>
-        <Text>Hey I'm {this.props.name}</Text>
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <Text>Name {context.state.name}</Text>
+              <Text>Age {context.state.age}</Text>
+              <Text>Cool {context.state.name}</Text>
+              <Button onPress={context.growYearOlder} title="grow a year" />
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
       </View>
-    )
+    );
   }
 }
 
-// the parenthesis let's us get ride of return  
-const Family = (props) => (
+// the parenthesis let's us get ride of return
+const Family = props => (
   <View>
-    <Person name={props.name}/>
+    <Person />
   </View>
-)
+);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
